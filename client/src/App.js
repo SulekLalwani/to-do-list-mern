7a86e3e206f3ac3c707189733455a9b91e50f792
Sulek Lalwani <73684484+SulekLalwani@users.noreply.github.com>
2023-02-01 23:15:08 +0530

    Add functionality to fetch the tasks array from the server and display it in the client

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState("Empty");
+
+  useEffect(() => {
+    fetch("http://localhost:5000/")
+      .then((res) => res.json())
+      .then((data) => {
+        setTasks(data.tasks);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{tasks}</p>
     </div>
   );
 }
