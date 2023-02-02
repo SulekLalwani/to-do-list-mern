07871d499a236ff8edeb8c1d9b520010bffc4e1c
Sulek Lalwani <73684484+SulekLalwani@users.noreply.github.com>
2023-02-02 06:12:29 +0530

    Add functionality to make POST requests to server to add new tasks

--- server/server.js
+++ server/server.js
@@ -2,10 +2,17 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const tasks = ["Task 1", "Task 2", "Task 3"];
 
 app.get("/", (req, res) => {
   res.json({ tasks: tasks });
+});
+
+app.post("/", (req, res) => {
+  tasks.push(req.body.newTask);
+  res.sendStatus(202);
 });
 
 app.listen(5000);
