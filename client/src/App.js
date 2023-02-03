7815151e4768e55dff576a16243302f14a8e967f
Sulek Lalwani <73684484+SulekLalwani@users.noreply.github.com>
2023-02-03 09:38:11 +0530

    Add functionality to edit tasks from client

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [tasks, setTasks] = useState(["Empty"]);
+  const [editingTask, setEditingTask] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:5000/")
@@ -10,6 +11,12 @@ function App() {
         setTasks(data.tasks);
       });
   }, []);
+
+  useEffect(() => {
+    const newEditingTask = [];
+    tasks.map(() => newEditingTask.push(false));
+    setEditingTask(newEditingTask);
+  }, [tasks]);
 
   function addTask(event) {
     event.preventDefault();
@@ -40,13 +47,65 @@ function App() {
     });
   }
 
+  function editTask(index) {
+    const newEditingTask = [];
+    tasks.map(() => newEditingTask.push(false));
+    newEditingTask[index] = true;
+    setEditingTask(newEditingTask);
+  }
+
+  function cancelEdit(index) {
+    const newEditingTask = [...editingTask];
+    newEditingTask[index] = false;
+    setEditingTask(newEditingTask);
+  }
+
+  function acceptEdit(event, index) {
+    event.preventDefault();
+    const editedTaskInput = event.target.elements.editTask;
+    fetch("http://localhost:5000", {
+      method: "PUT",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({ taskToEdit: index, edit: editedTaskInput.value }),
+    });
+
+    const newTasks = [...tasks];
+    newTasks[index] = editedTaskInput.value;
+    setTasks(newTasks);
+  }
+
   return (
     <div className="App">
       <ul>
         {tasks.map((task, index) => (
           <li>
-            <p>{task}</p>{" "}
-            <button onClick={() => deleteTask(index)}>Delete</button>
+            <div>
+              <p hidden={editingTask[index]}>{task}</p>
+              <button
+                hidden={editingTask[index]}
+                onClick={() => editTask(index)}
+              >
+                Edit
+              </button>{" "}
+              <button
+                hidden={editingTask[index]}
+                onClick={() => deleteTask(index)}
+              >
+                Delete
+              </button>
+            </div>
+            <form
+              onSubmit={(event) => {
+                acceptEdit(event, index);
+              }}
+              hidden={!editingTask[index]}
+            >
+              <input defaultValue={task} name="editTask"></input>{" "}
+              <button>Accept</button>{" "}
+              <button type="button" onClick={() => cancelEdit(index)}>
+                Cancel
+              </button>
+            </form>
           </li>
         ))}
       </ul>
