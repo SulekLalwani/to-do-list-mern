f3a52dd5000661be97fe972202387acc81ff36e1
Sulek Lalwani <73684484+SulekLalwani@users.noreply.github.com>
2023-02-12 17:49:55 +0530

    Use MongoDB as database for storing tasks

--- server/server.js
+++ server/server.js
@@ -1,33 +1,51 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.json());
 
-const tasks = ["Task 1", "Task 2", "Task 3"];
+mongoose.connect("mongodb://localhost/to-do-list-mern");
+
+const taskSchema = new mongoose.Schema({
+  tasks: [String],
+});
+
+const Tasks = mongoose.model("tasks", taskSchema);
 
 app.get("/", (req, res) => {
-  res.json({ tasks: tasks });
+  Tasks.findOne().then((tasks) => {
+    res.json({ tasks: tasks.tasks });
+  });
 });
 
 app.post("/", (req, res) => {
   if (req.body.newTask.length > 0) {
-    tasks.push(req.body.newTask);
-    res.sendStatus(202);
+    Tasks.updateOne({ $push: { tasks: req.body.newTask } }).then(() => {
+      res.sendStatus(202);
+    });
   } else {
     res.sendStatus(400);
   }
 });
 
 app.delete("/", (req, res) => {
-  tasks.splice(req.body.taskToDelete, 1);
-  res.sendStatus(200);
+  Tasks.updateOne({ $unset: { [`tasks.${req.body.taskToDelete}`]: "" } }).then(
+    () => {
+      Tasks.updateOne({ $pull: { tasks: null } }).then(() => {
+        res.sendStatus(200);
+      });
+    }
+  );
 });
 
 app.put("/", (req, res) => {
   if (req.body.edit.length > 0) {
-    tasks[req.body.taskToEdit] = req.body.edit;
-    res.sendStatus(200);
+    Tasks.updateOne({
+      $set: { [`tasks.${req.body.taskToEdit}`]: req.body.edit },
+    }).then(() => {
+      res.sendStatus(200);
+    });
   } else {
     res.sendStatus(400);
   }
