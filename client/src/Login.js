0cf47449f30e3b332360fa5c3abb821bb3848e8b
Sulek Lalwani <73684484+SulekLalwani@users.noreply.github.com>
2023-06-08 09:56:00 +0530

    Show a message in the login page if the credentials are incorrect

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./Login.css";
 
 export default function Login() {
+  const [invalidCredentials, setInvalidCredentials] = useState(false);
+
   async function logIn(event) {
     event.preventDefault();
 
     const username = event.target.elements.username.value;
     const password = event.target.elements.password.value;
 
-    await fetch("http://localhost:5000/login", {
+    const response = await fetch("http://localhost:5000/login", {
       method: "POST",
       headers: { "Content-type": "application/json" },
       body: JSON.stringify({ username: username, password: password }),
     });
+
+    if (response.status === 400) {
+      setInvalidCredentials(true);
+    }
   }
 
   return (
@@ -30,6 +36,9 @@ export default function Login() {
               name="password"
             />
           </div>
+          <p className="Message" hidden={!invalidCredentials}>
+            Username or password is incorrect
+          </p>
           <button>Log in</button>
         </div>
       </form>
